c5288d05bf4fa89c0d14c4625a38b27a88656e79
Benjamin Rochez <squiweb@gmail.com>
2016-10-27 16:43:41 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'pages/head.php' ?>
+
+<body>
+
+<header>
+  <h1 class="header__title">Welcome</h1>
+  <p class="header__p">
+    Codypack is ...
+  </p>
+</header>
+
+
+<?php include 'pages/end.php' ?>
